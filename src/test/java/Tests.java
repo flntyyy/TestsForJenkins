152072f14c6b4d396d201f4c867fbaee540cb524
alksndrsmrnv <flntyyy@icloud.com>
2020-01-11 21:32:50 +0300

    1 commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+
+
+    @Test
+    void addition() {
+        assertEquals(2, 1+1);
+    }
+
+
+    @Test
+    void additionError() {
+        assertEquals(14, 1+131);
+    }
+
+}
